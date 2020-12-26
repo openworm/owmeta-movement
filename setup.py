02aebbc77b0c383c20d63fb4bbed4fe0c9a9950c
Mark Watts <watts.mark2015@gmail.com>
2020-12-25 20:32:11 -0600

    Adding wcon schema to package_data

--- setup.py
+++ setup.py
@@ -4,4 +4,5 @@ setup(name='owmeta_movement',
       install_requires=[
           'owmeta_core',
           'sickle'],
+      package_data={'owmeta_movement': ['wcon_schema*.json']},
       packages=['owmeta_movement'])
