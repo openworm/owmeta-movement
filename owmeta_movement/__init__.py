a377551f22ccb62040bfeac2099fb244a86e542d
Mark Watts <watts.mark2015@gmail.com>
2020-12-25 20:33:16 -0600

    Correcting module for generated MovementDataSource classes

--- owmeta_movement/__init__.py
+++ owmeta_movement/__init__.py
@@ -2,13 +2,12 @@
 This example uses the WCON JSON-schema to build a DataSource type and then creates a new
 DataSource with the values from an instance conforming to the schema
 '''
-
-
 import json
+import importlib
 from owmeta_core.context import ClassContext
 from owmeta_core import BASE_CONTEXT
 from owmeta_core.json_schema import DataSourceTypeCreator
-from pkg_resources import Requirement, resource_filename
+from pkg_resources import resource_stream
 from rdflib.namespace import Namespace
 
 
@@ -21,21 +20,22 @@ CONTEXT = ClassContext(imported=(BASE_CONTEXT,),
                       base_namespace=BASE_SCHEMA_URL + '#')
 
 
-_wcon_schema_fname = resource_filename(Requirement.parse('owmeta_core'), 'wcon_schema_2017_06.json')
-with open(_wcon_schema_fname, 'r') as f:
-    schema = json.load(f)
-
-
-# The context of the DataSourceTypeCreator determines the definition context for the
-# classes created *unless* the schema definitions have an "$id", in which case the value
-# of the "$id" field takes precedence
 class MovementDataSourceTypeCreator(DataSourceTypeCreator):
+
+    def __init__(self):
+        wcon_schema = resource_stream('owmeta_movement', 'wcon_schema_2017_06.json')
+        with wcon_schema:
+            schema = json.load(wcon_schema)
+        super().__init__('MovementDataSource', schema, module=__name__, context=CONTEXT)
+
     def create_type(self, path, schema):
         self.cdict['base_namespace'] = Namespace(BASE_SCHEMA_URL)
         self.cdict['base_dat_namespace'] = Namespace(BASE_DATA_URL)
-        self.cdict['__module__'] = __name__
-        return super().create_type(path, schema)
+        res = super().create_type(path, schema)
+        mod = importlib.import_module(__name__)
+        setattr(mod, res.__name__, res)
+        res.register_on_module(mod)
+        return res
 
 
-ANNOTATED_SCHEMA = MovementDataSourceTypeCreator('MovementDataSource', context=CONTEXT).annotate(schema)
-MovementDataSource = ANNOTATED_SCHEMA
+ANNOTATED_SCHEMA = MovementDataSourceTypeCreator().annotate()
