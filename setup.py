49f9feeb3ced55c5125b40c49c75657760fb1cdf
Mark Watts <watts.mark2015@gmail.com>
2021-01-01 21:37:46 -0600

    Adding package requirements

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ from setuptools import setup
 setup(name='owmeta_movement',
       install_requires=[
           'owmeta_core',
-          'sickle'],
+          'rdflib',
+          'pow_zodb',
+          'requests',
+          'cachecontrol[filecache]'],
+      extras_require={'plot': ['matplotlib']},
       package_data={'owmeta_movement': ['wcon_schema*.json']},
       packages=['owmeta_movement'])
